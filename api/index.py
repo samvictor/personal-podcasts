7664a4a74ffe11445eed2d60edc79916db43ebfb
samvictor <sjinniss1@hotmail.com>
2024-10-17 21:45:14 -0400

    Avoiding writing to disk

--- api/index.py
+++ api/index.py
@@ -3,42 +3,16 @@ from dotenv import load_dotenv
 from pathlib import Path
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from flask import Flask, jsonify, send_file
-
-# Bark imports
-# from bark import SAMPLE_RATE, generate_audio, preload_models
-# from scipy.io.wavfile import write as write_wav
-
-# import io
-# import scipy.io.wavfile
+from flask import Flask, jsonify, send_file, Response
+import io
 
 # Load environment variables from .env.development.local file
 env_path = Path('.') / '.env.development.local'
 load_dotenv(dotenv_path=env_path)
 
-
-# download and load all Bark models
-# preload_models(
-#     text_use_small=True,
-#     coarse_use_small=True,
-#     fine_use_small=True,
-# )
-
-from pathlib import Path
 from openai import OpenAI
 openai_key = os.environ.get('OPENAI_KEY')
 client = OpenAI(api_key=openai_key)
-
-speech_file_path = Path(__file__).parent / "speech.mp3"
-response = client.audio.speech.create(
-  model="tts-1",
-  voice="alloy",
-  input="Today is a wonderful day to build something people love!"
-)
-
-response.stream_to_file(speech_file_path)
-
-
 
 app = Flask(__name__)
 
@@ -88,26 +62,13 @@ def api_example():
 
 @app.route("/api/speech")
 def get_speech():
-    if speech_file_path.exists():
-        return send_file(speech_file_path, mimetype="audio/mpeg")
-    else:
-        return jsonify({"error": "Speech file not found"}), 404
-
-# @app.route("/api/bark")
-# def bark():
-#     text_prompt = """
-#         Hello, my name is Suno. And, uh — and I like pizza. [laughs] 
-#         But I also have other interests such as playing tic tac toe.
-#     """
-#     audio_array = generate_audio(text_prompt)
-#     # write_wav("output.wav", SAMPLE_RATE, audio_array)
-#     # return send_file("output.wav")
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input="Today is a wonderful day to build something people love!"
+    )
     
-#     # Convert the audio array to WAV format
-#     wav_io = io.BytesIO()
-#     scipy.io.wavfile.write(wav_io, samplerate, audio_array)
-#     wav_io.seek(0)
-
-#     # Create a Flask Response with the WAV data
-#     return Response(wav_io.read(), mimetype="audio/wav")
-
+    audio_content = io.BytesIO(response.content)
+    audio_content.seek(0)
+    
+    return Response(audio_content, mimetype="audio/mpeg")
