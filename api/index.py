3a7cba188eca47a2d90898287031a0cd10761f5a
samvictor <sjinniss1@hotmail.com>
2024-10-23 12:49:44 -0400

    Splitting audio generation into 3 parts

--- api/index.py
+++ api/index.py
@@ -411,7 +411,7 @@ def eps_test():
         "script_text": this_ep.get("script_text")
         } for this_ep in eps]
     print(trimmed_eps)
-    return trimmed_eps
+    return eps
 
 
 # @app.route('/api/struct-test')
@@ -600,3 +600,204 @@ def new_episode():
     response.headers.set('Content-Type', 'application/rss+xml')
 
     return response
+
+
+ 
+@app.route('/api/new-script') 
+def new_script():
+    # generate script
+
+    
+    headline_articles = get_full_content_from_rss('https://abcnews.go.com/abcnews/topstories') 
+
+    headlines_section_of_text_for_ai = """ There should be a 'Headlines' section of the podcast.
+    During this section, have the characters talk about these articles. 
+    They are top headlines and the content of the article: """
+    for this_article in headline_articles:
+        headlines_section_of_text_for_ai += f"Title: {this_article.get("title")}, Content: {this_article.get("content")}. "
+    
+    
+    # sports_articles = get_full_content_from_rss('https://abcnews.go.com/abcnews/sportsheadlines') 
+
+    # sports_section_of_text_for_ai = """ There should be a 'Sports' section of the podcast.
+    # During this section, have the characters talk about these articles. 
+    # They are some latest sports articles: """
+    # for this_article in sports_articles:
+    #     sports_section_of_text_for_ai += f"Title: {this_article.get("title")}, Content: {this_article.get("content")}. "
+    
+    
+    tech_articles = get_full_content_from_rss('https://abcnews.go.com/abcnews/technologyheadlines') 
+
+    tech_section_of_text_for_ai = """ There should be a 'Tech' section of the podcast.
+    During this section, have the characters talk about these articles. 
+    They are some latest technology articles: """
+    for this_article in tech_articles:
+        tech_section_of_text_for_ai += f"Title: {this_article.get("title")}, Content: {this_article.get("content")}. "
+    
+    
+    entertainment_articles = get_full_content_from_rss('https://abcnews.go.com/abcnews/entertainmentheadlines') 
+
+    entertainment_section_of_text_for_ai = """ There should be a 'Entertainment' section of the podcast.
+    During this section, have the characters talk about these articles. 
+    They are some latest entertainment articles: """
+    for this_article in entertainment_articles:
+        entertainment_section_of_text_for_ai += f"Title: {this_article.get("title")}, Content: {this_article.get("content")}. "
+    
+
+
+    previous_eps = get_last_n_episodes(5)
+    # trimming eps to help ai focus on important content
+    trimmed_eps = [{
+        "created_at": this_ep.get("created_at"), 
+        "script_text": this_ep.get("script_text")
+        } for this_ep in previous_eps]
+    previous_eps_as_text = json.dumps(trimmed_eps, default=str)
+    previous_eps_section = """Here are the last few episodes. 
+    Look at the scripts and try not to repeat things from previous episodes. 
+    Also, feel free to make references to the things you talked about yesterday: """ + previous_eps_as_text
+
+
+
+    fun_facts_section = """ At the end there should be a Fun Fact section where they say something interesting or a fun fact about today.
+    If today is a holiday, they could wish the listener a happy [holiday] or national [xyz] day. 
+    Or they could mention something that happened on this day years ago.  """
+    
+    date_as_text = datetime.now(timezone).strftime("%B %d, %Y")
+    time_as_text = datetime.now(timezone).strftime("%H:%M")
+    datetime_as_text = date_as_text + " at " + time_as_text
+    
+    current_datetime_section = f" Today's date is {datetime_as_text}. "
+    
+    text_list = []
+    voice_list = []
+
+    podcast_response = message_ai_structured(directive 
+                             + headlines_section_of_text_for_ai
+                            #  + sports_section_of_text_for_ai
+                             + tech_section_of_text_for_ai
+                             + entertainment_section_of_text_for_ai 
+                             + current_datetime_section
+                             + fun_facts_section
+                             + previous_eps_section
+                            )
+    try:
+        podcast_script = podcast_response.script
+    except Exception:
+        print("error getting script from podcast:", podcast_response)
+        raise Exception("Error getting script from podcast:", podcast_response) 
+
+    # print("podcast script is", podcast_response.model_dump_json()) 
+
+    for script_line in podcast_script:
+        text_list.append(script_line.text)
+        voice_list.append(script_line.voice.lower())
+    
+
+    db_insert(table_name="episodes", data={
+        "podcast_id": 1, 
+        "user_id": "testUser",
+        "title": f"Daily Podcast for {date_as_text}", 
+        "description": f"This is your daily podcast for {date_as_text}. It was created at {time_as_text}", 
+        # "file_name": audio_file_name,
+        # "url": audio_url,
+        # "duration": audio_duration,
+        "script_text": podcast_response.model_dump_json(),
+        "audio_generated": False,
+    })
+    
+
+    return "<p>Audio generated</p>"
+
+
+@app.route("/api/make-audio-for-latest-ep")
+def latest_ep_audio():
+    latest_ep = get_last_n_episodes(1)[0]
+    if (latest_ep.get("audio_generated")):
+        # audio already generated
+        raise Exception("audio already generated for this episode")
+
+    text_list = []
+    voice_list = []
+
+    podcast_script = json.loads(latest_ep.get("script_text")).get("script")
+    # print("podcast script is", podcast_script) 
+    for script_line in podcast_script:
+        text_list.append(script_line.get("text"))
+        voice_list.append(script_line.get("voice").lower())
+        
+    audio_bytes = list(map(get_audio_bytes_from_text, text_list, voice_list))
+
+    delay_length_between_audio_clips = 2000
+    def fade_in_audio(audio_bytes):
+        # mute first and last 100 bytes of audio (set to 0)
+        # helps with combining clips without popping
+        # actually removing some kind of meta data from beginning
+        # can't do this to first clip
+
+        CHANGE_RANGE = 100
+        audio_length = len(audio_bytes)
+        if audio_length <= CHANGE_RANGE:
+            return audio_bytes
+        
+        audio_list = list(audio_bytes)
+
+        for i in range(0, CHANGE_RANGE):
+            # mute first 100 bytes
+            audio_list[i] = 0
+        #     # mute last 100 bytes
+            audio_list[audio_length - i - 1] = 0
+        
+        # add some empty space at the beginning
+        # to simulate a pause before speech
+        audio_list = [0] * delay_length_between_audio_clips + audio_list
+
+        return bytes(audio_list)
+    
+    def sum_bytes(bytes_list):
+        if not bytes_list or len(bytes_list) < 1:
+            raise ValueError("Bad input for bytes_list")
+        if len(bytes_list) == 1:
+            return bytes_list[0]
+
+        combined_bytes = bytes_list[0]
+        for i in range(1, len(bytes_list)):
+            combined_bytes += bytes_list[i]
+        
+        return combined_bytes
+
+
+    combined_audio_bytes = audio_bytes[0]
+    if len(audio_bytes) > 1:
+        combined_audio_bytes += sum_bytes( list(map(fade_in_audio, audio_bytes[1:])))
+    audio_duration  =0
+
+
+    audio_file_name = "daily_update_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".mp3"
+    file_path = "/audio/testUser/podcastId/" + audio_file_name
+
+    blob_response = vercel_blob.put(path=file_path,
+        data=combined_audio_bytes,
+        options={
+            "addRandomSuffix": "false",
+        })
+    
+    audio_url = vercel_blob_base + file_path
+
+    db_update(table_name="episodes", id=latest_ep.get("episode_id"), id_column_name="episode_id", data={
+        "file_name": audio_file_name,
+        "url": audio_url,
+        "duration": audio_duration,
+        "audio_generated": True,
+    })
+
+    # rss_text = generate_rss_text()
+    # # write the rss to a file in the blob storage
+    # vercel_blob.put(path='/rss/testUser/podcastId/testRss.xml', data=rss_text, options={
+    #             "addRandomSuffix": "false",
+    #         })
+    
+    # send the rss as a response
+    # response = make_response(rss_text)
+    # response.headers.set('Content-Type', 'application/rss+xml')
+
+    return "<p>Episode audio generated</p>"
