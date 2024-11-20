f16fac59057a4ba9e5584302f93a28e9e0d51dbe
samvictor <sjinniss1@hotmail.com>
2024-11-20 13:04:22 -0500

    Update page.tsx. Testing auto deploy on vercel

--- app/page.tsx
+++ app/page.tsx
@@ -127,6 +127,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
+          <p>version 0.1</p>
         </a>
       </div>
     </main>
