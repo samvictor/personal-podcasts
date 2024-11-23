f9236d880a2ecd02463ebd4f29de5758fb554d39
samvictor <sjinniss1@hotmail.com>
2024-11-23 00:24:05 -0500

    testing env variables

--- functions/main.py
+++ functions/main.py
@@ -4,10 +4,13 @@
 
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
+from firebase_functions.params import SecretParam
+
+testSecret = SecretParam('TEST_SECRET')
 
 initialize_app()
 
 
-@https_fn.on_request()
+@https_fn.on_request(secrets=[testSecret])
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+    return https_fn.Response("Hello world! Secret is: " + testSecret.value)
--- functions/venv/Scripts/activate.bat
+++ functions/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Sam\dev\personal-podcasts\app_hosting\functions\venv
+set VIRTUAL_ENV=C:\Users\Sam\dev\personal-podcasts\functions\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
