18e96bf7a20d9a2c94aff6b0ff4f720502fd7bbf
samvictor <sjinniss1@hotmail.com>
2024-11-22 22:56:29 -0500

    Testing firebase update

--- app_hosting/app/page.tsx
+++ app_hosting/app/page.tsx
@@ -128,7 +128,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-        Firebase! version 0.2
+        Firebase! version 0.3
       </div>
     </main>
   )
