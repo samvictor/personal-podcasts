8f6128f8d0ba01477a8c4bf27a25689d258264bd
samvictor <sjinniss1@hotmail.com>
2024-11-26 15:10:44 -0500

    Update index.py. Changing num lines in script from 60 to 50 so we can generate within Vercel time limit

--- api/index.py
+++ api/index.py
@@ -17,7 +17,7 @@ from typing import Literal
 
 DELAY_BETWEEN_NEWS_FETCH = 0.51
 NUM_ARTICLES_PER_CATEGORY = 3
-PODCAST_LENGTH = "60 lines"
+PODCAST_LENGTH = "50 lines"
 
 
 timezone = timezone('EST')
